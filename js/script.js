1443d1dcf42ff3187adb46edeafb205afc7d0e32
vrossi96 <vrossi396@yahoo.it>
2022-01-28 16:01:27 +0100

    comment

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ Rifare l'esercizio della to do list.
 |MILESTONE 3
 |Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 Bonus:
-1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+|1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
  */
 
