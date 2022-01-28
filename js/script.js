951a21050d1af6be165068c2144c5d4de85249c5
vrossi96 <vrossi396@yahoo.it>
2022-01-28 15:38:44 +0100

    comment

--- js/script.js
+++ js/script.js
@@ -5,10 +5,9 @@ Rifare l'esercizio della to do list.
 |- text, una stringa che indica il testo del todo
 |- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 |MILESTONE 1
-|Stampare all'interno di una lista, un item per ogni todo.
-|Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-MILESTONE 2
-Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+|Stampare all'interno di una lista, un item per ogni todo.Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+|MILESTONE 2
+|Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 Bonus:
