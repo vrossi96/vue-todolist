562c509940547216d06bb53c906cb23b2cff4645
vrossi96 <vrossi396@yahoo.it>
2022-01-28 16:48:48 +0100

    added checkbox in task list

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@ Rifare l'esercizio della to do list.
 |Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 |MILESTONE 3
 |Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
-Bonus:
+|Bonus:
 |1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
-2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+|2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
  */
 
 const root = new Vue({
