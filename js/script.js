7d4f543912e261023b44d1bdaba3e2f57953645b
vrossi96 <vrossi396@yahoo.it>
2022-01-28 15:18:02 +0100

    add dynamic list and class

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ Rifare l'esercizio della to do list.
 |Ogni todo sarà un oggetto, formato da due proprietà:
 |- text, una stringa che indica il testo del todo
 |- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-MILESTONE 1
-Stampare all'interno di una lista, un item per ogni todo.
-Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+|MILESTONE 1
+|Stampare all'interno di una lista, un item per ogni todo.
+|Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 MILESTONE 2
 Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 MILESTONE 3
@@ -22,6 +22,18 @@ const root = new Vue({
       tasks: [
          {
             text: "Comprare le verdure",
+            done: false,
+         },
+         {
+            text: "Recuperare l'auto dal meccanico",
+            done: false,
+         },
+         {
+            text: "Fare benzina",
+            done: true,
+         },
+         {
+            text: "Finire il libro",
             done: false,
          },
       ],
